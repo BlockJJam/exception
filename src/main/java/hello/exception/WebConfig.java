99d2ef3bf2161bf0e8f40b6e61f6b9b353aa396b
BlockJJam <blockjjam99@gmail.com>
2021-09-05 19:20:35 +0900

    feat: Spring MVC servlet exception과 filter, interception relation
    서블릿 오류 처리를 하는 과정에서 filter와 interceptor가 중복되는 설정을 핸들링해 보았다

---
+++ src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,36 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**","*.ico","/error"); // 오류 페이지 경로까지 추가
+    }
+
+    //@Bean
+    public FilterRegistrationBean logFilter(){
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+        //filterFilterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+        // -> 이 필터는 request, error 두 가지 경우에 사용된다라는 의미의 설정
+        return filterFilterRegistrationBean;
+    }
+
+}
